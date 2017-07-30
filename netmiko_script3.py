e68609a846609f740f490a67145cec518e0be727
Todd Riemenschneider <twr14152@gmail.com>
2017-07-30 10:58:59 -0400

    Create netmiko_script3.py
    Interactive script. User puts hosts and commands in when running script rather than creating separate files

---
+++ netmiko_script3.py
@@ -0,0 +1,47 @@
+#
+#This script will allow for user to interactively entering hosts and commands on the fly
+#
+#getpass will not display password
+from getpass import getpass
+#ConnectionHandler is the function used by netmiko to connect to devices
+from netmiko import ConnectHandler
+
+#create variables for username and password
+#create variables for configs and hosts
+uname = input("Username: ")
+passwd = getpass("Password: ")
+cmd = input("Enter config commands seperated by ',': ")
+host = input("Enter the host IPs seperate with space: ")
+
+#This will allow you to just press enter
+#This sets default values Not recommanded in any place but a lab
+if len(uname) < 1 : uname = "automate"
+if len(passwd) < 1 : passwd = "automation"
+
+#create lists for commands
+#This list can contain show or config commands show commands require "do + command"
+hosts = host.split()
+cmds = cmd.split(",")
+
+
+#use a for loop to iterate through the devices
+for host in hosts:
+    host_ip = host
+    ios_rtr = {
+        "device_type": "cisco_ios",
+        "ip": host_ip,
+        "username": uname,
+        "password": passwd,
+        }
+
+    #connect to the device via ssh
+    net_connect = ConnectHandler(**ios_rtr)
+    #print the device IP to identify which device is being configured
+    print("Connected to host:", host_ip)
+    #this variable is used to capture the output of cmds sent to device
+    output = net_connect.send_config_set(cmds)
+    #print the output
+    print(output)
+
+
+
