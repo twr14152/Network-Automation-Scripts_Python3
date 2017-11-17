aa68becc893dabdf1c2e695075a40907ed858ab0
Todd Riemenschneider <twr14152@gmail.com>
2017-11-17 11:22:19 -0500

    Create eapi_config_ex1.py

---
+++ eapi_client/eapi_config_ex1.py
@@ -0,0 +1,23 @@
+import pyeapi
+from pprint import pprint as pp
+
+host1 = pyeapi.connect_to("sw1")
+host2 = pyeapi.connect_to("sw2")
+
+devices = [host1, host2]
+
+value = 0
+
+for host in devices:
+    value += 1
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print("Pre-change State: ", host)
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    pp(host.enable(['show running-config']))
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print("Post-Change State:", host)
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    host.config(['interface loopback 0', 'ip address 1.1.1.{} 255.255.255.255'.format(value), 'description test', 'router ospf 1', 'network 0.0.0.0 255.255.255.255 area 0'])
+    pp(host.enable(['show running-config']))
+    
+
