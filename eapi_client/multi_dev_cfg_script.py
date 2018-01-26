8e0e083b3a552cef5e10243103b187bcbe1a293e
Todd Riemenschneider <twr14152@gmail.com>
2018-01-26 10:55:01 -0500

    Added exception handling

--- eapi_client/multi_dev_cfg_script.py
+++ eapi_client/multi_dev_cfg_script.py
@@ -11,22 +11,52 @@ commands1 = cmds1.split(',')
 commands2 = cmds2.split(',')
 commands3 = cmds3.split(',')
 
+err_host = []
+
 for host in devices:
     print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     print("Pre-change State: ", host)
-    node = pyeapi.connect_to(host)
-    pp(node.enable(['show running-config']))
-    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    try:
+        node = pyeapi.connect_to(host)
+        pp(node.enable(['show running-config']))
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    except Exception as unknown_error:
+        print("************************************")
+        print("Unable to log into this device:", host)
+        print(unknown_error)
+        err_host.append(host)
+        print("Errors logging into:", err_host)
+        print("************************************")
+        continue
     if host == 'veos1':
-        pp(node.config(commands1))
-        print("Post-change state: ", host)
-        pp(node.enable(['show running-config']))
+        try:
+            pp(node.config(commands1))
+            print("Post-change state: ", host)
+            pp(node.enable(['show running-config']))
+        except Exception as cmd_error:
+            print("Command not working, make sure syntax is correct")
+            print("Problem with commands on: ", host)
+            print(cmd_error)
+            continue
     elif host == 'veos2':
-        pp(node.config(commands2))
-        print("Post-change state: ", host)
-        pp(node.enable(['show running-config']))
+        try:
+            pp(node.config(commands1))
+            print("Post-change state: ", host)
+            pp(node.enable(['show running-config']))
+        except Exception as cmd_error:
+            print("Command not working, make sure syntax is correct")
+            print("Problem with commands on: ", host)
+            print(cmd_error)
+            continue
     elif host == 'veos3':
-        pp(node.config(commands3))
-        print("Post-change state: ", host)
-        pp(node.enable(['show running-config']))
+        try:
+            pp(node.config(commands1))
+            print("Post-change state: ", host)
+            pp(node.enable(['show running-config']))
+        except Exception as cmd_error:
+            print("Command not working, make sure syntax is correct")
+            print("Problem with commands on: ", host)
+            print(cmd_error)
+            continue
        
+print('Problems connecting to these hosts: ',  err_host)
