c32daf097770c4cc482b854e322ea639fe12bf5a
Todd Riemenschneider <twr14152@gmail.com>
2018-03-05 07:43:49 -0500

    Create show_commands_v2.py

---
+++ netmiko/NetworkDiscovery/show_commands_v2.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+# (c) 2017 Todd Riemenschneider
+#This script will allow up to update a group of hosts with common config shared by all
+#Enable Multiprocessing 
+from multiprocessing import Pool
+#getpass will not display password
+from getpass import getpass
+#ConnectionHandler is the function used by netmiko to connect to devices
+from netmiko import ConnectHandler
+#Time tracker
+from time import time
+
+#create variables for username and password
+#create variables for configs and hosts
+uname = input("Username: ")
+passwd = getpass("Password: ")
+cmd = input("Enter show command: ")
+host = input("Enter the host IPs seperate with space: ")
+
+#This will allow you to just press enter
+#This sets default values Not recommanded in any place but a lab
+if len(uname) < 1 : uname = "admin"
+if len(passwd) < 1 : passwd = "automate"
+
+#create lists of hosts and cmds to iterate through
+#This list can contain show or config commands show commands require "do + command"
+hosts = host.split()
+
+starting_time = time()
+
+#Each member of the pool of 5 will be run through this function
+def run_script(host_ip):
+    ios_rtr = {
+        "device_type": "cisco_ios",
+        "ip": host_ip,
+        "username": uname,
+        "password": passwd,
+        }
+    #connect to the device via ssh
+    net_connect = ConnectHandler(**ios_rtr)
+    #print the device IP to identify which device is being configured
+    print("Connected to host:", host_ip)
+    #this variable is the show command entered
+    output = net_connect.send_command(cmd)
+    #print the output
+    print(output)
+    print('\n---- Elapsed time=', time()-starting_time)
+
+if __name__ == "__main__":
+    # Pool(5) means 5 process / devices will be run at a time, until youve gone through the device list
+    with Pool(5) as p:
+        print(p.map(run_script, hosts))
