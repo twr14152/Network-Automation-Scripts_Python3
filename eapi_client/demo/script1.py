4a77ad052168ca8863347baabbdd9d84b3ab9258
Todd Riemenschneider <twr14152@gmail.com>
2018-03-25 12:33:32 -0400

    Create script1.py

---
+++ eapi_client/demo/script1.py
@@ -0,0 +1,62 @@
+import pyeapi
+from pprint import pprint as pp
+
+node1 = pyeapi.connect_to('veos5')
+node2 = pyeapi.connect_to('veos6')
+
+node1.api("ipinterfaces").create("Ethernet1")
+node2.api("ipinterfaces").create("Ethernet1")
+
+node1.config("ip name-server 8.8.8.8")
+node1.config("ip route 0.0.0.0/0 192.168.1.1")
+
+node2.config("ip name-server 8.8.8.8")
+node2.config("ip route 0.0.0.0/0 192.168.1.1")
+
+pp(node1.run_commands("ping cisco.com"))
+pp(node2.run_commands("ping cisco.com"))
+
+cmds = ["show version", "show running-config", "show management api http-commands"]
+
+pp(node1.run_commands(cmds))
+pp(node2.run_commands(cmds))
+
+configurations1 = [
+"ip routing",
+"interface Ethernet1",
+"ip address 10.0.0.1/24",
+"no shutdown",
+"interface loopback 1",
+"ip address 1.1.1.1/32",
+"router ospf 1",
+"network 10.0.0.0 0.0.0.255 area 0",
+"network 1.1.1.1 0.0.0.0 area 0",
+"router-id 1.1.1.1",
+]
+
+configurations2 = [
+"ip routing",
+"interface Ethernet1",
+"ip address 10.0.0.2/24",
+"no shutdown",
+"interface loopback 1",
+"ip address 2.2.2.2/32",
+"router ospf 1",
+"network 10.0.0.0 0.0.0.255 area 0",
+"network 2.2.2.2 0.0.0.0 area 0",
+"router-id 2.2.2.2",
+]
+
+
+node1.config(configurations1)
+node2.config(configurations2)
+
+pingHost = ["ping 1.1.1.1", "ping 2.2.2.2"]
+
+print("#" * 45)
+print("Printing results of ping to loopbacks advertised via ospf")
+pp(node1.run_commands(pingHost))
+
+print("#" * 45)
+print("Printing results of ping to loopbacks advertised via ospf")
+pp(node2.run_commands(pingHost))
