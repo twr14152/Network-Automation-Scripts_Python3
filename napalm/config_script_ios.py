dbe9b91b610adcf973e70952361230b0471f44bf
Todd Riemenschneider <twr14152@gmail.com>
2018-05-30 13:51:42 -0400

    Create config_script_ios.py

---
+++ napalm/config_script_ios.py
@@ -0,0 +1,20 @@
+import json
+from napalm import get_network_driver
+from pprint import pprint as pp
+from multiprocessing import Pool
+
+routers = ['r1', 'r2', 'r3', 'r4', 'r5']
+
+def run_script(router):
+    driver = get_network_driver('ios')
+    rtr = driver(router, 'admin', 'automate')
+    rtr.open()
+    print("Connected to: ", (rtr.hostname))
+    #pp(rtr.device.send_command('show run'))
+    pp(rtr.device.send_config_from_file('/home/todd/automation/napalm_stuff/ACL.cfg'))
+
+
+if __name__ == "__main__":
+    with Pool(5) as p:
+        print(p.map(run_script, routers))
+    
