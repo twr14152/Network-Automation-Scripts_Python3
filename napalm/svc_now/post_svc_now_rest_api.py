84108633369c3bb59d684a88381898456453f257
Todd Riemenschneider <twr14152@gmail.com>
2018-06-18 22:52:39 -0400

    Update post_svc_now_rest_api.py

--- napalm/svc_now/post_svc_now_rest_api.py
+++ napalm/svc_now/post_svc_now_rest_api.py
@@ -6,7 +6,7 @@ from pprint import pprint as pp
 user = ''
 pwd = getpass("enter password: " )
 
-with open('host_file') as f:
+with open('host_file2') as f:
     host_list = f.read().splitlines()
 
 print(host_list)
@@ -14,18 +14,16 @@ print(host_list)
 for host in host_list:
     with open('snow_device_facts_'+ host + '.json', 'r', encoding='utf-8') as device:
         # Set the request parameters
-        url = 'https://<server>.service-now.com/api/now/table/cmdb_ci_netgear'
+        url = 'https://<>.<>.com/api/adx/external_ci_import/createCI'
+        #url = 'https://<>.<>.com/api/now/table/cmdb_ci_netgear'
         # Set proper headers
         headers = {"Content-Type":"application/json","Accept":"application/json"}
         # Do the HTTP request
         print(device)
         print(host)
         response = requests.post(url, auth=(user, pwd), headers=headers , data=device)
-        # Check for HTTP codes other than 200
-        if response.status_code != 200: 
-            print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())            
-            continue
-        # Decode the JSON response into a dictionary and use the data
+        # Check for HTTP codes 
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
         data = response.json()
         print(data)
 
