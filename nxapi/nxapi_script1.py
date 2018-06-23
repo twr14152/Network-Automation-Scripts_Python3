98a8d326c1e2375608a9af6e32c82abb9a482a88
Todd Riemenschneider <twr14152@gmail.com>
2018-06-23 00:48:36 -0400

    Interactive configuration and show command script

---
+++ nxapi/nxapi_script1.py
@@ -0,0 +1,37 @@
+import requests
+import json
+import getpass
+from pprint import pprint as pp
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+UN = 'admin'
+PW = 'Admin_1234!'
+
+commands = input('Commands seperate with ",":  ')
+cmds = commands.split(',')
+
+url='https://64.103.37.14/ins'
+print(url)
+
+myheaders={'content-type':'application/json-rpc'}
+payload = []
+instance_id = 0
+for command in cmds:
+    instance_id += 1
+    payload_dict = {
+        "jsonrpc": "2.0",
+        "method": "cli",
+        "params": {
+          "cmd": command,
+          "version": 1
+        },
+        "id": instance_id,
+      }
+    payload.append(payload_dict)
+    pp(command)
+
+response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(UN,PW),verify=False).json()
+pp(response)
+
