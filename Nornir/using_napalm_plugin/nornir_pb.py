56675690ebe920e4638099dea569fbf75e0fd07c
Todd Riemenschneider <twr14152@gmail.com>
2018-09-05 09:45:16 -0400

    Create nornir_pb.py

---
+++ Nornir/using_napalm_plugin/nornir_pb.py
@@ -0,0 +1,16 @@
+from nornir.core import InitNornir                                                                                                                                                            
+from nornir.plugins.tasks import commands
+from nornir.plugins.functions.text import print_result
+from nornir.plugins.tasks import networking
+
+nr = InitNornir(config_file="config.yaml")
+
+lab_hosts = nr.filter(site="lab", role="spline")
+
+result = lab_hosts.run(task=networking.napalm_get,
+                       getters=["facts"])
+
+print_result(result)
+
+print(nr.inventory.hosts)
+print(nr.inventory.groups)
