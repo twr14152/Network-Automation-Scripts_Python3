d146f333b5c98589f32556e854877f33e1c7f53f
Todd Riemenschneider <twr14152@gmail.com>
2018-11-12 12:39:36 -0500

    Create script1.py

---
+++ Nornir/beta2.0.0b1/script1.py
@@ -0,0 +1,15 @@
+from nornir import InitNornir                                                                                                                   
+from nornir.plugins.functions.text import print_result
+from nornir.plugins.tasks import networking
+
+nr = InitNornir(config_file="config.yaml")
+
+lab_hosts = nr.filter(role="spline")
+
+result = lab_hosts.run(task=networking.napalm_get,
+                       getters=["facts", "config", "interfaces_ip"])
+
+print_result(result)
+
+print(nr.inventory.hosts)
+print(nr.inventory.groups)
