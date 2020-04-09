c5cdaebe983e0fe29614b33458e69ba6e2b2328b
Todd Riemenschneider <twr14152@gmail.com>
2020-04-09 09:42:31 -0500

    Create netconf_config_bkup.py

---
+++ netconf/netconf_config_bkup.py
@@ -0,0 +1,27 @@
+from ncclient import manager
+import xml.dom.minidom
+import sys 
+
+HOSTS = ['ios-xe-mgmt-latest.cisco.com', 'ios-xe-mgmt.cisco.com']
+PORT = 10000
+USER = 'developer'
+PASS = 'C1sco12345'
+
+
+def main():
+    for HOST in HOSTS:
+        with manager.connect(host=HOST, port=PORT, username=USER,
+                         password=PASS, hostkey_verify=False,
+                         device_params={'name': 'default'},
+                         look_for_keys=False, allow_agent=False) as m:
+            print(HOST, "- running-config copied ")
+            config = m.get_config('running')
+            xml_data = xml.dom.minidom.parseString(config.xml)
+            router_configs = xml_data.toprettyxml(indent = "  ")
+            #print(router_configs)
+            with open("config_bkups/" + HOST + "_running_config.txt", 'w') as file:
+                file.write(router_configs)
+ 
+
+if __name__ == "__main__":
+    main()
