24e3076829922ec2c2a9a0d6b9de6efab93ba29a
Todd Riemenschneider <twr14152@gmail.com>
2023-09-04 09:24:44 -0400

    Create get_data_dashboard.py

---
+++ requests/meraki/get_data_dashboard.py
@@ -0,0 +1,149 @@
+#!/usr/bin/python3
+
+import requests
+import json
+from pprint import pprint as pp
+
+print('''
+        Example:
+        Enter Org ID: 573083052582915264
+        Enter Network ID: L_573083052582991362
+        Enter API_KEY: 6bec40cf957de430a6f1f2baa056b99a4fac9ea0
+        ''')
+
+
+orgId = input("Enter Org ID: ")
+# set default values
+if len(orgId) == 0 : orgId = "1097053"
+
+networkId = input("Enter Network ID: ")
+# set default values
+if len(networkId) == 0 : networkId = "L_627126248111380187"
+
+
+API_KEY = input("Enter API_KEY: " )
+# set default values
+if len(API_KEY) == 0 : API_KEY = "40bfc95138a521bffc6691b2ee0be63b1d0da812"
+
+url_0 = f"https://api.meraki.com/api/v1/organizations/"
+url_1 = f"https://api.meraki.com/api/v1/networks/{networkId}"
+url_2 = f"https://api.meraki.com/api/v1/organizations/{orgId}/networks"
+
+
+payload = {}
+
+
+headers = {
+    "Accept": "application/json",
+    "X-Cisco-Meraki-API-Key": f"{API_KEY}",
+}
+urls = [url_0, url_1,url_2]
+i = 0
+for url in urls:
+    i +=1
+    print(f"Loop {i}")
+    try:
+        response = requests.request("GET", url, headers=headers, data=payload)
+        data = json.loads(response.text)
+        print(json.dumps(data, indent = 2))
+    except Exception as e:
+        print(e)
+
+
+
+
+'''
+OutPut
+$ ./get_data_dashboard.py
+
+        Example:
+        Enter Org ID: 573083052582915264
+        Enter Network ID: L_573083052582991362
+        Enter API_KEY: 6bec40cf957de430a6f1f2baa056b99a4fac9ea0
+
+Enter Org ID:
+Enter Network ID:
+Enter API_KEY:
+Loop 1
+[
+  {
+    "id": "1097053",
+    "name": "Independent Consultant",
+    "url": "https://n114.meraki.com/o/7QEucc/manage/organization/overview",
+    "api": {
+      "enabled": true
+    },
+    "licensing": {
+      "model": "co-term"
+    },
+    "cloud": {
+      "region": {
+        "name": "North America"
+      }
+    },
+    "management": {
+      "details": []
+    }
+  }
+]
+Loop 2
+{
+  "id": "L_627126248111380187",
+  "organizationId": "1097053",
+  "productTypes": [
+    "switch"
+  ],
+  "url": "https://n114.meraki.com/Gibsons_Network-/n/MVk3rbYb/manage/usage/list",
+  "name": "Gibsons_Network",
+  "timeZone": "America/New_York",
+  "enrollmentString": null,
+  "tags": [],
+  "notes": "",
+  "isBoundToConfigTemplate": false
+}
+Loop 3
+[
+  {
+    "id": "L_627126248111380187",
+    "organizationId": "1097053",
+    "name": "Gibsons_Network",
+    "productTypes": [
+      "switch"
+    ],
+    "timeZone": "America/New_York",
+    "tags": [],
+    "enrollmentString": null,
+    "url": "https://n114.meraki.com/Gibsons_Network-/n/MVk3rbYb/manage/usage/list",
+    "notes": "",
+    "isBoundToConfigTemplate": false
+  },
+  {
+    "id": "N_627126248111476956",
+    "organizationId": "1097053",
+    "name": "Jacks_Room",
+    "productTypes": [
+      "switch"
+    ],
+    "timeZone": "America/Los_Angeles",
+    "tags": [],
+    "enrollmentString": null,
+    "url": "https://n114.meraki.com/Jacks_Room/n/qSdrCcYb/manage/usage/list",
+    "notes": null,
+    "isBoundToConfigTemplate": false
+  },
+  {
+    "id": "N_627126248111476958",
+    "organizationId": "1097053",
+    "name": "Evee_Network",
+    "productTypes": [
+      "wireless"
+    ],
+    "timeZone": "America/Los_Angeles",
+    "tags": [],
+    "enrollmentString": null,
+    "url": "https://n114.meraki.com/Evee_Network/n/HgaoccYb/manage/usage/list",
+    "notes": null,
+    "isBoundToConfigTemplate": false
+  }
+]
+'''
